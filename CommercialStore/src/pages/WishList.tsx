99d70f9136a9dfc0abe6bb7f5144df295b439553
kalia <kaliarovich93@gmail.com>
2023-11-01 23:33:31 +0200

    feat: add 'removeItem' feature for wishList. Add counter for wishlist icon

--- CommercialStore/src/pages/WishList.tsx
+++ CommercialStore/src/pages/WishList.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import Helmet from "../components/Helmet/Helmet";
-import { TypedUseSelectorHook, useSelector } from "react-redux";
+import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import { RootState } from "../redux/store";
 import s from "../style/pages/WishList.module.scss";
 import { IoMdClose } from "react-icons/io";
 import Sidebar from "../components/SIdebar/Sidebar";
 import EmptyWishlist from "../components/UI/Empty wishlist/EmptyWishlist";
+import { ItemProps, wishListProduct } from "../interfaces/DataInterfaces";
+import { wishListActions } from "../redux/slices/wishListSlice";
+import { toast } from "react-toastify";
 
 export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
 
 export default function WishList() {
+  const dispatch = useDispatch();
   const wishListItems = useAppSelector((state) => state.wishList.wishListItems);
+
+
+
   return (
     <Helmet title="WishList">
       <div className={s.wrapper}>
@@ -21,32 +28,42 @@ export default function WishList() {
         <section className={s.wishList}>
           <h2>Wishlist</h2>
           <div className={s.products}>
-            {wishListItems.length !== 0
-              ? wishListItems.map((item, index) => {
-                  return (
-                    <div className={s.product}>
-                      <button className={s.delete}>
-                        <IoMdClose />
-                      </button>
-                      <img src={item.img} alt={item.name} />
-                      <div className={s.productDesc}>
-                        <p>{item.name}</p>
-                        <p>
-                          Price for one: <span>{item.price}$</span>
-                        </p>
-                        <p>
-                          Quntity: <span>{item.quantity}</span>
-                        </p>
-                      </div>
-                      <div className={s.price}>
-                        Total price: <br />
-                        <span>{item.totalPrice}$</span>
-                      </div>
-                      <button className={s.addBtn}>Add to cart</button>
-                    </div>
+            {wishListItems.length !== 0 ? (
+              wishListItems.map((item, index) => {
+
+                const removeFromWishList = () => {
+                  dispatch(
+                    wishListActions.removeItem(item.id)
                   );
-                })
-              : <EmptyWishlist/>}
+              
+                  toast.success("Product removed from your wish list");
+                };
+                return (
+                  <div className={s.product}>
+                    <button className={s.delete} onClick={removeFromWishList}>
+                      <IoMdClose />
+                    </button>
+                    <img src={item.img} alt={item.name} />
+                    <div className={s.productDesc}>
+                      <p>{item.name}</p>
+                      <p>
+                        Price for one: <span>{item.price}$</span>
+                      </p>
+                      <p>
+                        Quntity: <span>{item.quantity}</span>
+                      </p>
+                    </div>
+                    <div className={s.price}>
+                      Total price: <br />
+                      <span>{item.totalPrice}$</span>
+                    </div>
+                    <button className={s.addBtn}>Add to cart</button>
+                  </div>
+                );
+              })
+            ) : (
+              <EmptyWishlist />
+            )}
           </div>
         </section>
       </div>
