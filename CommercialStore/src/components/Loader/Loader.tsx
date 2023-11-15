3dc19eb90bdb0bb4a0515ce83e3b9b44db473fef
kalia <kaliarovich93@gmail.com>
2023-11-15 23:12:18 +0200

    feat: add styles for Loader

--- CommercialStore/src/components/Loader/Loader.tsx
+++ CommercialStore/src/components/Loader/Loader.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import { SpinnerCircular } from 'spinners-react/lib/esm/SpinnerCircular'
-
+import React from "react";
+import { SpinnerDotted } from "spinners-react/lib/esm/SpinnerDotted";
+import s from './Loader.module.scss'
 
 export default function Loader() {
   return (
-    <div><SpinnerCircular/></div>
-  )
+    <div className={s.wrapper}>
+      <SpinnerDotted size={100} color={"rgba(138, 51, 253, 1)"} />
+    </div>
+  );
 }
--- CommercialStore/src/components/UI/ProductList/ProductList.tsx
+++ CommercialStore/src/components/UI/ProductList/ProductList.tsx
@@ -1,12 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useProducts } from "../../../hooks/useProducts";
 import Helmet from "../../Helmet/Helmet";
 import s from "./ProductList.module.scss";
 import { MdFavoriteBorder } from "react-icons/md";
-import { Link } from "react-router-dom";
+import Loader from "../../Loader/Loader";
 
 export default function ProductList() {
+  const [loading, setLoading] = useState(false);
   const products = useProducts();
+
+  useEffect(() => {
+    if (products.length > 0) {
+      setLoading(true);
+    }
+  }, [products]);
+  
   return (
     <Helmet title={"Shop"}>
       <div className={s.wrapper}>
@@ -18,7 +26,7 @@ export default function ProductList() {
         </section>
 
         <section className={s.productsList}>
-          {products.map((item) => {
+          {loading ? products.map((item) => {
             return (
               <div className={s.product} key={item.id}>
                 <img src={item.img} alt={item.title} />
@@ -34,7 +42,7 @@ export default function ProductList() {
                 </div>
               </div>
             );
-          })}
+          }) : <Loader/>}
         </section>
       </div>
     </Helmet>
