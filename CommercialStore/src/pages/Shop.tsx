607fb049116fac6a84afb43cd1d7ee2f93e85991
kalia <kaliarovich93@gmail.com>
2023-11-17 22:57:45 +0200

    feat: add 'shop description' section and styles for it

--- CommercialStore/src/pages/Shop.tsx
+++ CommercialStore/src/pages/Shop.tsx
@@ -39,7 +39,6 @@ export default function Shop() {
     <Helmet title={"Shop"}>
       {loading ? (
         <div className={s.wrapper}>
-          
           <section className={s.filters}>
             <div className={s.priceFilter}>
               <div id="rangeSlider">reerer</div>
@@ -69,6 +68,38 @@ export default function Shop() {
           </div>
 
           <ProductList products={productsData} />
+
+          <section className={s.shopDescr}>
+            <h2 className={s.sectionTitle}>
+              Why you should choise our online store?
+            </h2>
+            <p className={s.descr}>
+              Welcome to the world of stylish shopping – welcome to Euohoria
+              online store! We've created this unique store with love for style,
+              beauty, and convenience to make your shopping experience
+              unforgettable. Euohoria is not just a store; it's a guide to the
+              world of fashion and beauty. We take pride in offering our
+              customers a curated selection of the best products from leading
+              global brands. Here, you'll find everything you need to create
+              your stylish look, whether it's everyday clothing, accessories, or
+              beauty products. Our virtual shelves are filled with only the
+              finest offers, and we guarantee that every purchase will bring you
+              joy. Navigating the Euohoria website is easy and intuitive,
+              providing you with convenience when choosing products and placing
+              orders. Every detail of our store is designed with your comfort in
+              mind. We provide detailed product descriptions, size information,
+              and high-quality images so you can make informed choices. We also
+              offer secure payment systems and efficient delivery services to
+              ensure your order is processed and delivered as quickly as
+              possible. Euohoria is not just a store; it's a community of
+              like-minded individuals who love beauty and style. Join us on
+              social media to stay updated on the latest trends, exclusive
+              offers, and inspiring style stories. Immerse yourself in the
+              fascinating world of Euohoria, where beauty is in every detail. We
+              invite you to enjoy the perfect online shopping experience at
+              Euohoria. Your style is our priority!
+            </p>
+          </section>
         </div>
       ) : (
         <Loader />
