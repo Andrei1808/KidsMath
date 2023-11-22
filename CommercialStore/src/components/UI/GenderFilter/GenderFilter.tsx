7d0aa627b9dd8a9385386815938b8f44ec714514
kalia <kaliarovich93@gmail.com>
2023-11-23 00:20:48 +0200

    fix: change input name, for grouping inputs in one group 'category'

--- CommercialStore/src/components/UI/GenderFilter/GenderFilter.tsx
+++ CommercialStore/src/components/UI/GenderFilter/GenderFilter.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import s from "./GenderFilter.module.scss";
+import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 
 interface GenderFilterProps {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -10,17 +11,30 @@ export const GenderFilter: React.FC<GenderFilterProps> = ({
   onChange,
   selectedCategory,
 }) => {
+  const [isVisible, setIsVisible] = useState(true);
+console.log(selectedCategory);
   return (
     <div className={s.genderFilter}>
-      <label className={selectedCategory === "new" ? s.active : ""}>
-        <input type="radio" name="male" value="men" onChange={onChange} />
-        Men
-      </label>
+      <h4
+        onClick={() => {
+          setIsVisible(!isVisible);
+        }}
+      >
+        Gender <span>{isVisible ? <IoIosArrowDown /> : <IoIosArrowUp />}</span>
+      </h4>
+      {isVisible && (
+        <div className={s.filterValues}>
+          <label className={ ""}>
+            <input type="radio" name="category" value="men" onChange={onChange} />
+            Men
+          </label>
 
-      <label className={selectedCategory === "all" ? s.active : ""}>
-        <input type="radio" name="male" value="women" onChange={onChange} />
-        Women
-      </label>
+          <label className={""}>
+            <input type="radio" name="category" value="women" onChange={onChange} />
+            Women
+          </label>
+        </div>
+      )}
     </div>
   );
 };
